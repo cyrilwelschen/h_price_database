91f3e0e4a9ba48ed42ed26dfdaff59d99302ec60
cyrilwelschen <cj.welschen@gmail.com>
2018-12-26 16:41:13 +0100

    set up very basic concept of std scrap routines

--- h_price_database/std_scrap_routines.py
+++ h_price_database/std_scrap_routines.py
@@ -2,3 +2,16 @@
 File contains standard scraping routines/functions. E.g. to search for button and press if found.
 """
 
+
+class Analyser:
+
+    def __init__(self):
+        self.name = "Analyser from std_scrap_routine.py"
+        self.version = "0.0.1"
+
+
+class Explorer:
+
+    def __init__(self):
+        self.name = "Explorer from std_scrap_routine.py"
+        self.version = "0.0.1"
