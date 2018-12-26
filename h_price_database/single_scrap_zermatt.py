8fb3d8acd09b69bab6836f309cc57521fc5d8d45
cyrilwelschen <cj.welschen@gmail.com>
2018-12-26 19:52:14 +0100

    working example of scraping from zermatt

--- h_price_database/single_scrap_zermatt.py
+++ h_price_database/single_scrap_zermatt.py
@@ -5,18 +5,30 @@ File containing functionality to scrap zermatt.ch for hotel prices
 from selenium import webdriver
 from xvfbwrapper import Xvfb
 from h_price_database.logger import Logger
+from time import sleep
+from lxml import html
 
 
 log = Logger()
 
 
 def parse(url):
-    print("Starting")
-    log.log("Starting")
-
+    # log.log("Starting")
     response = webdriver.Chrome('../drivers/chromedriver')
     response.get(url)
     print(response.current_url)
+    # todo: periodically check if search is finised and then go on instead of sleeping for 10 sec
+    sleep(10)
+
+    parser = html.fromstring(response.page_source, response.current_url)
+    hotels_on_page = parser.xpath('//div[@class="media ng-scope"]')
+    if hotels_on_page:
+        log.log(len(hotels_on_page))
+        for hotel in hotels_on_page:
+            hotel_name = hotel.xpath('.//span[@ng-bind="house.lis_name"]')
+            hotel_location = hotel.xpath('.//span[@ng-bind="house.lis_city"]')
+            hotel_price = hotel.xpath('.//span[contains(@ng-bind, "house.lis_price")]')
+            print(hotel_name[0].text_content(), hotel_location[0].text_content(), hotel_price[0].text_content())
 
 
 def run(check_in_date, check_out_date):
