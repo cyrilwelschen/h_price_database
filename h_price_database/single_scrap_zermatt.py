490cf8d1a8d3ea22bb5e500958ed0183af9fa351
cyrilwelschen <cj.welschen@gmail.com>
2018-12-26 16:17:36 +0100

    very simple first scrap of zermatt.ch

--- h_price_database/single_scrap_zermatt.py
+++ h_price_database/single_scrap_zermatt.py
@@ -1,3 +1,26 @@
 """
 File containing functionality to scrap zermatt.ch for hotel prices
 """
+
+from selenium import webdriver
+from xvfbwrapper import Xvfb
+from h_price_database.logger import Logger
+
+
+log = Logger()
+
+
+def parse(url):
+    print("Starting")
+    log.log("Starting")
+
+    response = webdriver.Chrome('../drivers/chromedriver')
+    response.get(url)
+    print(response.current_url)
+
+
+if __name__ == "__main__":
+    vdisplay = Xvfb()
+    vdisplay.start()
+    parse("https://www.zermatt.ch/en/book")
+    vdisplay.stop()
--- h_price_database/std_scrap_routines.py
+++ h_price_database/std_scrap_routines.py
@@ -1,3 +1,4 @@
 """
 File contains standard scraping routines/functions. E.g. to search for button and press if found.
 """
+
